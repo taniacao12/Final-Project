7e16739aa01c842f8ea3fe2faad7d5c609ca8906
taniacao12 <tcaosu@stuy.edu>
2019-01-11 11:48:03 -0500

    fix new line in fget

--- client.c
+++ client.c
@@ -7,17 +7,22 @@ int main() {
   from_server = client_handshake( &to_server );
   char data[BUFFER_SIZE];
   char user[BUFFER_SIZE];
-  
+  char start[BUFFER_SIZE];
   // welcome user and get their username
   printf("WELCOME TO TEAMANCALA!\n");
+  // get user name
   printf("What is your name? ");
   fgets(user, BUFFER_SIZE, stdin);
+  user[strlen(user) - 1] = 0;
   // give user the game instructions
   printf("Hi %s! Here are the rules to the game:\n", user);
   // rules
   // insert rules here
   printf("If you are ready, press ENTER to start the game.");
-  fgets(user, BUFFER_SIZE, stdin);
+  fgets(start, BUFFER_SIZE, stdin);
+  printf("------------------------------------------------\n");
+
+  char board[12];
   while (1) {
     // get user input
     printf("ENTER MESSAGE: ");
